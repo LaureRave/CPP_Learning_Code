a931815204a0de13bc406a2f762ce97966ed22f9
Laure Ravelomanantsoa <ravelomanantso@edu.univ-eiffel.fr>
2024-01-20 01:49:49 +0100

    parrot

--- chap-01/2-parrot.cpp
+++ chap-01/2-parrot.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 int main()
 {
-    std::cout << "Craow" << std::endl;
+    std::string line;
+    // std::vector<std::string> lines;
+    std::getline(std::cin, line);
+    std::stringstream lines;
+
+    while (!line.empty())
+    {
+        // lines.push_back(line);
+        lines << "Craow " << line << std::endl;
+        std::getline(std::cin, line);
+    }
+
+    /*for (auto l : lines)
+    {
+        std::cout << "Craow " << l << std::endl;
+    }*/
+    std::cout << lines.str();
+
     return 0;
 }

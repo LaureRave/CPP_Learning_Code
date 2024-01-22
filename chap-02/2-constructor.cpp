b975a83988dbf75caa6ed501260736a623b8c657
Ravelomanantsoa Laure <ravelomanantso@edu.univ-eiffel.fr>
2024-01-22 11:56:04 +0100

    constructor

--- chap-02/2-constructor.cpp
+++ chap-02/2-constructor.cpp
@@ -4,25 +4,36 @@
 class Person
 {
 public:
-    std::string  get_name() const { return _name; }
+
+    Person(std::string name, std::string surname)
+        : _name {name}
+        , _surname {surname}
+    {
+        //std::cout << "Person named " << name << " has just been born!" << std::endl;
+    }
+
+    std::string  get_fullname() const { return _name + " " + _surname; }
     unsigned int get_age() const { return _age; }
 
-    void set_name(std::string name) { _name = name; }
-    void set_age(unsigned int age) { _age = age; }
+    //void set_name(std::string name) { _name = name; }
+    //void set_age(unsigned int age) { _age = age; }
+    void wait(unsigned int years) { _age+=years; }
 
 private:
     std::string  _name;
+    std::string _surname;
     unsigned int _age = 0u;
 };
 
 int main()
 {
-    Person p;
+    Person p {"Brune", "Wayne"};
 
-    p.set_name("Batman");
-    p.set_age(23);
+    //p.set_name("Batman");
+    //p.set_age(23);
+    p.wait(23);
 
-    std::cout << "Person named '" << p.get_name() << "' is " << p.get_age() << " years old." << std::endl;
+    std::cout << "Person named '" << p.get_fullname() << "' is " << p.get_age() << " years old." << std::endl;
 
     return 0;
 }

b6d36356dc7e9ce5349321c37d2c8234d4342a4d
Ravelomanantsoa Laure <ravelomanantso@edu.univ-eiffel.fr>
2024-01-22 12:20:28 +0100

    destructor

--- chap-02/3-destructor.cpp
+++ chap-02/3-destructor.cpp
@@ -9,6 +9,11 @@ public:
         , _surname { surname }
     {}
 
+    ~Person()
+    {
+        std::cout << get_full_name() << " died at " << get_age() << " years old" << std::endl;
+    }
+
     std::string  get_full_name() const { return _name + " " + _surname; }
     unsigned int get_age() const { return _age; }
 
@@ -20,11 +25,30 @@ private:
     unsigned int _age = 0u;
 };
 
+class Batmobile
+{
+    private:
+    Person _batman {"Brune", "Wayne"};
+    
+    public:
+    Batmobile() 
+    {
+        _batman.wait(23);
+    }
+
+    ~Batmobile() {
+        std::cout << "The Batmobile has been destroyed!" << std::endl;
+    }
+};
+
 int main()
 {
-    {
+    /*{
         Person batman { "Bruce", "Wayne" };
         batman.wait(23);
+    }*/
+    {
+        Batmobile batmobile;
     }
 
     std::cout << "After block" << std::endl;

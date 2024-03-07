8d0f267ac1bdf7d8c73ac652141b5ad2dca06b3e
Ravelomanantsoa Laure <ravelomanantso@edu.univ-eiffel.fr>
2024-03-07 15:19:19 +0100

    conteneurs polymorphes

--- chap-04/2-farm/FarmHouse.cpp
+++ chap-04/2-farm/FarmHouse.cpp
@@ -4,12 +4,27 @@
 #include "Cow.h"
 #include "Dog.h"
 
+#include <vector>
+
 void sing_a_lot(const Animal& animal, unsigned int times)
 {
-    while (times-- > 0)
+    while (times > 0)
     {
-        animal.sing();
+        while (--times > 0) 
+        {
+            animal.sing(' ');
+        }
+        animal.sing('\n');
     }
+}
+
+void sing_chorus(const std::vector<Animal*>& animals) 
+{
+    for (const auto* animal : animals) 
+    {
+        animal->sing(' ');
+    }
+    std::cout << std::endl;
 }
 
 int main()
@@ -24,6 +39,12 @@ int main()
     sing_a_lot(cow, 3u);
     sing_a_lot(dog, 1u);
     sing_a_lot(chicken, 2u);
+
+    std::vector<Animal*> animals;
+    animals.push_back(&dog);
+    animals.push_back(&cat);
+
+    sing_chorus(animals);
 
     return 0;
 }

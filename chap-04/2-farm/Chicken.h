5798d96dba4705c5459ba940abf60c7722fe3755
Laure Ravelomanantsoa <ravelomanantso@edu.univ-eiffel.fr>
2024-03-10 22:00:19 +0100

    destructeur chicken

--- chap-04/2-farm/Chicken.h
+++ chap-04/2-farm/Chicken.h
@@ -5,5 +5,7 @@
 class Chicken : public Animal
 {
 public:
+    ~Chicken() { std::cout << "CotCooooooooot!" << std::endl; }
+
     void sing(char next_char) const override { std::cout << "Cotcotcotcodet" << next_char; }
 };

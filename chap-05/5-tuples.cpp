e7d38749f8541b0f3b1920a1f4d581f5d1aedd96
Ravelomanantsoa Laure <ravelomanantso@edu.univ-eiffel.fr>
2024-03-25 12:32:38 +0100

    paires et tuples

--- chap-05/5-tuples.cpp
+++ chap-05/5-tuples.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <string>
+#include <tuple>
 
-bool parse_params(int argc, const char* const* argv, int& numerator, int& denominator)
+std::tuple<bool, int, int> parse_params(int argc, const char* const* argv)
 {
+    const std::tuple<bool, int, int> error { false, 0, 0 };
     if (argc != 3)
     {
         std::cerr << "Program expects 2 parameters!" << std::endl;
-        return false;
+        return error;
     }
 
     std::string num_str = argv[1];
     std::string den_str = argv[2];
+
+    int numerator   = 0;
+    int denominator = 0;
 
     try
     {
@@ -20,37 +25,37 @@ bool parse_params(int argc, const char* const* argv, int& numerator, int& denomi
     catch (const std::exception&)
     {
         std::cerr << "Program expects 2 integer parameters!" << std::endl;
-        return false;
+        return error;
     }
 
     if (denominator == 0)
     {
         std::cerr << "Denominator cannot be null!" << std::endl;
-        return false;
+        return error;
     }
 
-    return true;
+    return std::tuple { true, numerator, denominator };
 }
 
-int divide(int numerator, int denominator, int& reminder)
+std::tuple<int, int> divide(int numerator, int denominator)
 {
-    reminder = numerator % denominator;
-    return numerator / denominator;
+    return std::tuple { numerator / denominator, numerator % denominator };
 }
 
 int main(int argc, char** argv)
 {
-    int numerator   = 0;
-    int denominator = 0;
+    const auto res = parse_params(argc, argv);
 
-    if (!parse_params(argc, argv, numerator, denominator))
+    if (!std::get<0>(res))
     {
         return 1;
     }
 
-    int       reminder = 0;
-    const int quotient = divide(numerator, denominator, reminder);
-
+    const int  numerator   = std::get<1>(res);
+    const int  denominator = std::get<2>(res);
+    const auto div         = divide(numerator, denominator);
+    const int  quotient    = std::get<0>(div);
+    const int  reminder    = std::get<1>(div);
     std::cout << numerator << " = " << denominator << " * " << quotient << " + " << reminder << std::endl;
 
     return 0;

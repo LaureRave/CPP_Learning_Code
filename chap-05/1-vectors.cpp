6b12bb84646f92183db71a750930a1d92658cf10
Ravelomanantsoa Laure <ravelomanantso@edu.univ-eiffel.fr>
2024-03-25 12:33:56 +0100

    m

--- chap-05/1-vectors.cpp
+++ chap-05/1-vectors.cpp
@@ -1,8 +1,8 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
-void display(std::vector<int> vector) 
+void display(std::vector<int> vector)
 {
     for (const auto v : vector)
     {
@@ -11,19 +11,18 @@ void display(std::vector<int> vector)
     std::cout << std::endl;
 }
 
-
 int main()
 {
     // CONSTRUCTION
     std::vector<int> vec(4, 0);
     display(vec);
 
-    int array[] = { 0, 1, 2, 3, 4, 5 };
+    int  array[]                 = { 0, 1, 2, 3, 4, 5 };
     int* iterator_on_2nd_element = &array[1];
 
     std::vector<int> vec2(&array[2], &array[5]);
     display(vec2);
-    
+
     std::vector<int> vec3(&array[0], &array[6]);
 
     std::vector<int> sol(array, array + 6);
@@ -32,12 +31,12 @@ int main()
 
     // ACCES AUX ELEMENTS
     std::vector<int> full(array, array + 6);
-    int size = full.size();
-    
-    for (int i=0; i<size / 2; i++)
+    int              size = full.size();
+
+    for (int i = 0; i < size / 2; i++)
     {
-        int temp = full[i];
-        full[i] = full[size - 1 - i];
+        int temp           = full[i];
+        full[i]            = full[size - 1 - i];
         full[size - 1 - i] = temp;
     }
 
@@ -48,20 +47,19 @@ int main()
 
     display(full);
 
-    for (auto it=full.begin(); it != full.end(); it+=2) 
+    for (auto it = full.begin(); it != full.end(); it += 2)
     {
         it = full.emplace(it, *it);
     }
 
     display(full);
 
-    int array2[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int              array2[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     std::vector<int> vec4(array2, array2 + 11);
 
     vec4.erase(vec4.begin() + 2, vec4.begin() + 7);
 
     display(vec4);
-
 
     auto it = std::find(vec4.begin(), vec4.end(), 9);
 
